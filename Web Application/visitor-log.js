381e2e9755a4d051843f1c07c5ebaadb8ef70e1d
Yan Ting <101209183@students.swinburne.edu.my>
2021-04-10 22:48:28 +0800

    Update visitor-log.js

--- Web Application/visitor-log.js
+++ Web Application/visitor-log.js
@@ -7,23 +7,26 @@ var t = $('#visitor-log').DataTable({
 
 db.collection("visitor").get().then((querySnapshot) => {
   querySnapshot.forEach((doc) => {
-	  var checkin_time;
-	  var checkout_time;
-	  if(doc.data().checkin_time == null)
-		  checkin_time = null;
-	  else
-	  	checkin_time = doc.data().checkin_time.toDate().toLocaleString();
-	  if(doc.data().checkout_time == null)
-		  checkout_time = null;
-	  else
-		  checkout_time= doc.data().checkout_time.toDate().toLocaleString();
+	var checkin_time;
+	var checkout_time;
+	
+	if(doc.data().checkin_time == null)
+		checkin_time = null;
+	else
+	checkin_time = doc.data().checkin_time.toDate().toLocaleString();
+	if(doc.data().checkout_time == null)
+		checkout_time = null;
+	else
+		checkout_time= doc.data().checkout_time.toDate().toLocaleString();
+
       t.row.add([
         doc.data().username,
 		doc.data().ic,
         doc.data().contact,
         checkin_time,
 		checkout_time,
-		doc.data().status
+		doc.data().status,
+		expire_time
       ]).draw();
   });
 });
