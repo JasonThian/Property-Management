25a1358341a86a97f5a061f480e6ebd5a76e9bf5
Yan Ting <101209183@students.swinburne.edu.my>
2021-04-14 16:48:46 +0800

    Update announcement.php

--- Web Application/announcement.php
+++ Web Application/announcement.php
@@ -54,11 +54,12 @@ if(isset($_FILES['annc_image']['name'])){
 </head>
 
 <body>
-	<div class="sidebar">
-		<header><img src="images/dryx-black.png" alt="dryx-logo" width="50%"></header>
+	<div class="sidebar">		
 		<ul>
+			<img src="images/dryx-logo.png" alt="dryx-logo">
 			<li><a href="residents.html"><i class="fas fa-user-friends"></i>Residents</a></li>
 			<li><a href="livechat.html"><i class="fas fa-comment-dots"></i>Live Chat</a></li>
+			<li><a href="update-bill.html"><i class="fas fa-bolt"></i>Update bill</a></li>
 			<li><a href="payment-log.html"><i class="fas fa-money-bill-alt"></i>Payment Log</a></li>
 			<li><a href="visitor-log.html"><i class="fas fa-address-card"></i>Visitor Log</a></li>
 			<li><a href="announcement.html"><i class="fas fa-bell"></i>Announcements</a></li>
