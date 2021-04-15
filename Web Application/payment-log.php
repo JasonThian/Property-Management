f8bce10f64b0eb5b177e605f8a34e2127431e728
Yan Ting <101209183@students.swinburne.edu.my>
2021-04-15 17:04:41 +0800

    Update payment-log.php

--- Web Application/payment-log.php
+++ Web Application/payment-log.php
@@ -6,9 +6,9 @@
 	<link rel="stylesheet" href="style/style.css"/>
 	<script src="https://kit.fontawesome.com/a076d05399.js"></script>
 	<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-	<script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+	<script src="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css"></script>
+	<script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
 	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.css">
-	<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.js"></script>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 
@@ -18,9 +18,9 @@
 	?>
 	
 	<div class="content">
-		<h1>Payment Log</h1>
+		<h1 class="page-title">Payment Log</h1>
 		
-		<table id="payment-log" class="table hover order-column">
+		<table id="payment-log" class="table table-sm hover order-column dt-responsive nowrap">
 			<thead>
 				<tr>
 					<th>User Name</th>
@@ -33,15 +33,15 @@
 			</thead>
 			
 			<tbody id="list">
-			</tbody>
+			</tbody>	
 		</table>
-		
+
 		<div class="pagination-container">
 			<nav>
-				<ul class="pagination"></ul>
+				<ul class="pagination">
+				</ul>
 			</nav>	
 		</div>
-			
 	</div>
 	<!-- The core Firebase JS SDK is always required and must be listed first -->
 <script src="https://www.gstatic.com/firebasejs/7.21.1/firebase-app.js"></script>
