761a14eb6de843d96e3c788120317576aaf22455
Yan Ting <101209183@students.swinburne.edu.my>
2021-04-15 17:04:39 +0800

    Update navbar.php

--- Web Application/navbar.php
+++ Web Application/navbar.php
@@ -8,9 +8,11 @@
 			<li><a href="livechat.php"><i class="fas fa-comment-dots"></i>Live Chat</a></li>
 			<li><a href="update-bill.php"><i class="fas fa-bolt"></i>Update bill</a></li>
 			<li><a href="payment-log.php"><i class="fas fa-money-bill-alt"></i>Payment Log</a></li>
+			<li><a href="bookings.php"><i class="fas fa-building"></i>Facility Booking Log</a></li>
 			<li><a href="visitor-log.php"><i class="fas fa-address-card"></i>Visitor Log</a></li>
-			<li><a href="announcement.php"><i class="fas fa-bell"></i>Announcements</a></li>
-			<li><a href="bookings.php"><i class="fas fa-building"></i>Facility bookings</a></li>
+			<li><a href="facility.php"><i class="fas fa-building"></i>Facility</a></li>
+			<li><a href="announcement.php"><i class="fas fa-bell"></i>Announcements</a></li>			
+			<li><a href="issue-report.php"><i class="fas fa-bell"></i>Issue Report</a></li>	
 			<li><a href="login.php" id="logout"><i class="fas fa-sign-out-alt"></i>Logout</a></li>
 		</ul>
 	</div>
