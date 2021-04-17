9824010497b45ef895052124589a485330ce0767
Yan Ting <101209183@students.swinburne.edu.my>
2021-04-17 14:11:21 +0800

    Update login.php

--- Web Application/login.php
+++ Web Application/login.php
@@ -4,9 +4,8 @@
 <meta charset="utf-8">
 <title>Login - Project Management System</title>
 <link href="style/style.css" rel="stylesheet" type="text/css">
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>	
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>	
 </head>
 
 <body class="loginpage">
