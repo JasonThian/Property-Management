1ecaa7cb0d7d2fedecffd43e1588376b23475b6b
JasonThian <100086673@students.swinburne.edu.my>
2021-05-03 00:26:08 +0800

    live update booking, payment and visitor log

--- visitor-log.js
+++ visitor-log.js
@@ -29,6 +29,49 @@ db.collection("visitor").get().then((querySnapshot) => {
         checkin_time,
 		checkout_time,
 		doc.data().status
-      ]).draw();
+      ]).node().id = doc.id;
+	  t.draw();
   });
+}).then(() => {
+	var listening = db.collection("visitor").onSnapshot(async(snapshot)=>{
+		
+		await snapshot.docChanges().forEach(async(change)=>{
+			var checkin_time;
+			var checkout_time;
+			
+			if(change.doc.data().checkin_time == null)
+				checkin_time = null;
+			else
+				checkin_time = change.doc.data().checkin_time.toDate().toLocaleString();
+			if(change.doc.data().checkout_time == null)
+				checkout_time = null;
+			else
+				checkout_time= change.doc.data().checkout_time.toDate().toLocaleString();
+			
+			var query = null;
+			
+			try{
+				query = [change.doc.data().username,change.doc.data().ic,change.doc.data().contact,change.doc.data().carplate,checkin_time,checkout_time,change.doc.data().status];
+			}catch(err){
+				console.log(err);
+			}
+			
+			try{
+				t.row("#"+change.doc.id).data(query).draw();
+			}catch(err){
+				t.row.add(query).node().id = change.doc.id;
+				t.draw();
+			}
+		});
+		
+	},(error)=>{
+		console.log(error);
+	});
+
+	window.onbeforeunload = function(){
+		listening();
+	}
+}).catch((err) => {
+	console.log(err);
 });
+
